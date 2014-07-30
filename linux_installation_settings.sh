b61ee4d04e125de794ec3032f9e5792d87df412c
pranaysharma <me@pranays.com>
2014-07-30 19:20:48 +0800

    Updated Changes
    1. Apt-fast
    2. NEmo file manager
    3. Linux header files
    4. Option to add Aliases
    5. set correct option to set fn key off for multimedia

--- linux_installation_settings.sh
+++ linux_installation_settings.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #The MIT License (MIT)
 #
 #Copyright (c) 2014 pranaysharma(me@pranays.com)
@@ -20,12 +21,40 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
+###### set the root password ######
+sudo passwd root
+
+###### change the user to root for installation of all the code below  #####
+sudo su
+
 ##### basic scrypt ####
 sudo apt-get update; sudo apt-get upgrade -y
 
+### install and initialize git #####
+git config --global user.name "Pranay Sharma"
+git config --global user.email "me@pranays.com"
+
+####apt-fast installation #####
+sudo apt-get -y install aria2 git
+git clone https://github.com/ilikenwf/apt-fast.git
+cd apt-fast
+cp apt-fast /usr/bin/
+chmod +x /usr/bin/apt-fast
+cp apt-fast.conf /etc
+## doesnt work with adding the repo as 14.04 is not supported it seems
+#sudo add-apt-repository -y ppa:apt-fast/stable
+#sudo apt-get update && sudo apt-get install -y apt-fast
+###########################################################
+
+###### basic bash setup. Source startup-engineering class https://d396qusza40orc.cloudfront.net/startup/lecture_slides%2Flecture4b-developer-environment.pdf
+git clone https://github.com/startup-class/setup.git
+./setup/setup.sh
 
 #### broadcom wireless & fan control #####
 sudo apt-get install -y bcmwl-kernel-source macfanctld
+
+#### install linux headers and source ####
+apt-get install build-essential linux-source linux-headers-`uname -r`
 
 #### disable online search for Dash ######
 wget -q -O - https://fixubuntu.com/fixubuntu.sh | bash
@@ -35,6 +64,17 @@ gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 #### Enable overlay scrollbars ######
 #gsettings reset com.canonical.desktop.interface scrollbar-mode
+
+#### install nemo file manager:http://www.webupd8.org/2013/10/install-nemo-with-unity-patches-and.html #####
+sudo add-apt-repository -y ppa:webupd8team/nemo
+sudo apt-get update
+sudo apt-get install -y nemo nemo-fileroller
+   ### To prevent Nautilus from handling the desktop icons (and use
+   ### Nemo instead), use the commands below:
+	sudo apt-get install -y dconf-tools
+	gsettings set org.gnome.desktop.background show-desktop-icons false
+   ### Set Nemo as the default file manager (replacing Nautilus)
+	xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 
 
 #### Enable Firewall #####
@@ -57,6 +97,7 @@ nano /etc/default/tlp
 #DISK_IDLE_SECS_ON_BAT=2
 #MAX_LOST_WORK_SECS_ON_BAT=60
 #CPU_SCALING_GOVERNOR_ON_BAT=powersave
+#CPU_BOOST_ON_BAT=0
 #DISK_APM_LEVEL_ON_BAT="1 1"
 #RUNTIME_PM_ALL=1
 #RESTORE_DEVICE_STATE_ON_STARTUP=1
@@ -71,7 +112,7 @@ sudo add-apt-repository ppa:poliva/lightum-mba && sudo apt-get update && sudo ap
 #####   Auto screen  brightness   http://www.matrix44.net/blog/?p=963
 
 
-##### essential for programming 
+##### essential for programming
 sudo apt-get update && apt-get upgrade -y && apt-get install -y build-essential gcc make cmake mc python-dev subversion git bzip2 unzip xorg python2.7 python-numpy python-scipy libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm libtool libboost-all-dev python-setuptools libxss-dev libdbus-glib-1-dev openjdk-7-jre freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
 
 ##### initial setup files
@@ -81,8 +122,9 @@ sudo apt-get install -y terminator screen flashplugin-installer ubuntu-restricte
 sudo apt-get install -y nautilus-open-terminal
 
 ##### Skype #####
-sudo apt-get install -y libqt4-dbus libqt4-network libqt4-xml libasound2 && wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64
+sudo apt-fast install -y libqt4-dbus libqt4-network libqt4-xml libasound2 && wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64
 sudo dpkg -i getskype-* && sudo apt-get -f install
+sudo apt-fast install -y sni-qt:i386
 
 #### FTP and server related ####
 sudo apt-get install -y filezilla filezilla-common
@@ -114,12 +156,16 @@ sudo add-apt-repository -y ppa:n-muench/vlc
 sudo apt-get update
 sudo apt-get install -y vlc mplayer
 
+#### Install Banshee player for mpr ####
+sudo apt-fast install -y banshee
+
 ##### Other Todo ######
 #1. Enable Workspaces and desktop launch icon:http://askubuntu.com/questions/260510/how-do-i-turn-on-workspaces-why-do-i-only-have-one-workspace-since-13-04/287408#287408
 
 
 ##### Disable Function key for multimedia(set to 1 to reenable) #####
-echo 2 > /sys/module/hid_apple/parameters/fnmode
+sudo su
+sudo echo "echo 2 > /sys/module/hid_apple/parameters/fnmode" >>  /etc/init.d/rc.local &
 
 
 ##### reset Unity ######
@@ -132,13 +178,13 @@ echo 2 > /sys/module/hid_apple/parameters/fnmode
 cv+cuda  http://sn0v.wordpress.com/2012/05/11/installing-cuda-on-ubuntu-12-04/
 cv       http://karytech.blogspot.tw/2012/05/opencv-24-on-ubuntu-1204.html
 ##### opencv dependencies ##########
-sudo apt-get install -y build-essential checkinstall cmake pkg-config yasm 
+sudo apt-get install -y build-essential checkinstall cmake pkg-config yasm
 ##related to image i/o
 sudo apt-get install -y libtiff4-dev libjpeg-dev libjasper-dev
 ## related to video i/o
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
 
-### related to python 
+### related to python
 sudo apt-get install -y python-dev python-numpy
 #### 3rd party libraries
 sudo apt-get install -y libtbb-dev
@@ -147,7 +193,10 @@ sudo apt-get install -y libqt4-dev libgtk2.0-dev
 
 
 
-
+##setting up aliases
+	#for the workspace
+	#workspace=/media/pranay/6A88-A62A/workspace
+	#alias workspace="cd $workspace"
 
 
 
