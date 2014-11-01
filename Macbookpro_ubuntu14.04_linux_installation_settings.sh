871213a6dff7130e99367a1dfc1a49365e1ee209
pranaysharma <me@pranays.com>
2014-11-01 13:48:12 +0530

    Add description

--- Macbookpro_ubuntu14.04_linux_installation_settings.sh
+++ Macbookpro_ubuntu14.04_linux_installation_settings.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#description : This file is a collection of commands containing all the commands and settings 
+#required for setting up Ubuntu 14.04 on MacbookPro 9,1. The final aim of this file is to be 
+#changed into a single scrypt which would run un-attended and setup a new system
+
 #The MIT License (MIT)
 #
 #Copyright (c) 2014 pranaysharma(me@pranays.com)
